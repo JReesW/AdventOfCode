af9d5c1da2b979bef8ae22233d184f3b50f4851d
John Williams <j.rees.w@gmail.com>
2022-12-05 11:54:01 +0100

    Update day05.py

--- day05.py
+++ day05.py
@@ -1,14 +1,15 @@
-stacks = [
-    ['F', 'D', 'B', 'Z', 'T', 'J', 'R', 'N'],
-    ['R', 'S', 'N', 'J', 'H'],
-    ['C', 'R', 'N', 'J', 'G', 'Z', 'F', 'Q'],
-    ['F', 'V', 'N', 'G', 'R', 'T', 'Q'],
-    ['L', 'T', 'Q', 'F'],
-    ['Q', 'C', 'W', 'Z', 'B', 'R', 'G', 'N'],
-    ['F', 'C', 'L', 'S', 'N', 'H', 'M'],
-    ['D', 'N', 'Q', 'M', 'T', 'J'],
-    ['P', 'G', 'S']
-]
+def get_stacks():
+    return [
+        ['F', 'D', 'B', 'Z', 'T', 'J', 'R', 'N'],
+        ['R', 'S', 'N', 'J', 'H'],
+        ['C', 'R', 'N', 'J', 'G', 'Z', 'F', 'Q'],
+        ['F', 'V', 'N', 'G', 'R', 'T', 'Q'],
+        ['L', 'T', 'Q', 'F'],
+        ['Q', 'C', 'W', 'Z', 'B', 'R', 'G', 'N'],
+        ['F', 'C', 'L', 'S', 'N', 'H', 'M'],
+        ['D', 'N', 'Q', 'M', 'T', 'J'],
+        ['P', 'G', 'S']
+    ]
 
 
 with open("addenda/input05.txt", 'r') as file:
@@ -16,12 +17,40 @@ with open("addenda/input05.txt", 'r') as file:
 
 
 def part1():
+    stacks = get_stacks()
+
     for line in lines:
         line, dest = line.strip().split(" to ")
         num, src = line.split("move ")[1].split(" from ")
         num, src, dest = int(num), int(src), int(dest)
 
+        stacks[src - 1], stack = stacks[src - 1][:-num], stacks[src - 1][-num:]
+        stacks[dest - 1] += list(reversed(stack))
 
+    res = ""
+    for stack in stacks:
+        res += stack[-1]
+
+    return res
+
+
+def part2():
+    stacks = get_stacks()
+
+    for line in lines:
+        line, dest = line.strip().split(" to ")
+        num, src = line.split("move ")[1].split(" from ")
+        num, src, dest = int(num), int(src), int(dest)
+
+        stacks[src - 1], stack = stacks[src - 1][:-num], stacks[src - 1][-num:]
+        stacks[dest - 1] += stack
+
+    res = ""
+    for stack in stacks:
+        res += stack[-1]
+
+    return res
 
 
 print(part1())
+print(part2())
