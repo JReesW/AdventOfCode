5ba40220c3385685860cb2512a7249c01f1bbda1
JReesW <j.rees.w@gmail.com>
2022-12-07 14:48:27 +0100

    refactored day 1

--- 2022/day01.py
+++ 2022/day01.py
@@ -1,16 +1,29 @@
 with open("addenda/input01.txt", 'r') as file:
     lines = file.readlines()
 
-elf = 1
-total = []
-current = []
 
-for line in lines:
-    if line.strip().isnumeric():
-        current.append(int(line))
-    else:
-        total.append(current)
-        current = []
+def prep():
+    total = []
+    current = []
+
+    for line in lines:
+        if line.strip().isnumeric():
+            current.append(int(line))
+        else:
+            total.append(current)
+            current = []
+    return total
 
 
-print(sorted([sum(t) for t in total], reverse=True))
+def part1():
+    total = prep()
+    return sorted([sum(t) for t in total], reverse=True)[0]
+
+
+def part2():
+    total = prep()
+    return sum(sorted([sum(t) for t in total], reverse=True)[:3])
+
+
+print(part1())
+print(part2())
