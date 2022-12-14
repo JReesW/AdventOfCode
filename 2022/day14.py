92f97366c4d9a68b89865ddff5ba7a5a4d6252cb
JReesW <j.rees.w@gmail.com>
2022-12-14 08:01:25 +0100

    14 start

---
+++ 2022/day14.py
@@ -0,0 +1,25 @@
+with open("addenda/test.txt", 'r') as file:
+    lines = file.readlines()
+
+
+def part1():
+    paths = []
+    for line in lines:
+        paths.append([[int(c) for c in coords.split(',')] for coords in line.split(' -> ')])
+    xmax, xmin, ymax, ymin = -99999, 99999, -99999, 99999
+    for path in paths:
+        for coord in path:
+            x, y = coord
+            xmax = x if x > xmax else xmax
+            xmin = x if x < xmin else xmin
+            ymax = y if y > ymax else ymax
+            ymin = y if y < ymin else ymin
+
+
+
+def part2():
+    pass
+
+
+print(part1())
+print(part2())
