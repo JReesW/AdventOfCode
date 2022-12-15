8d209c4baba1ec187bbad21c67b3aa3ecee51597
JReesW <j.rees.w@gmail.com>
2022-12-15 08:14:46 +0100

    15 start

---
+++ 2022/day15.py
@@ -0,0 +1,31 @@
+with open("addenda/test.txt", 'r') as file:
+    lines = file.readlines()
+
+
+def dist(a, b):
+    return abs(a[0] - a[1]) + abs(b[0] - b[1])
+
+
+def part1():
+    beacons = []
+    sensors = []
+
+    for line in lines:
+        match line.split():
+            case [_, _, xs, ys, _, _, _, _, xb, yb]:
+                xs = int(xs.split(',')[0].split('=')[1])
+                ys = int(ys.split(':')[0].split('=')[1])
+                xb = int(xb.split(',')[0].split('=')[1])
+                yb = int(yb.split('=')[1])
+                beacons.append((xb, yb))
+                sensors.append((xs, ys))
+
+
+
+
+def part2():
+    pass
+
+
+print(part1())
+print(part2())
