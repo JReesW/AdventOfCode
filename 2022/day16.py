aa92dcb1c0a912c00a72a227d7e8aa4512162f38
JReesW <j.rees.w@gmail.com>
2022-12-17 01:40:07 +0100

    16 finished thank fuck

--- 2022/day16.py
+++ 2022/day16.py
@@ -1,4 +1,5 @@
 import re
+import multiprocessing as mp
 
 
 with open("addenda/input16.txt", 'r') as file:
@@ -6,12 +7,13 @@ with open("addenda/input16.txt", 'r') as file:
 
 
 class Valve:
-    def __init__(self, name, flow, conns):
+    def __init__(self, name, flow, conns, max_time):
         self.name = name
         self.flow = flow
         self.conns = conns
         self.valves = None
         self.dists = {}
+        self.max_time = max_time
 
     def __repr__(self):
         return f"{self.name}, {self.flow}f/m, conn: {[(k.name, d) for k, d in self.dists.items()]}"
@@ -28,17 +30,17 @@ class Valve:
     def search(self, visited, time, score):
         results = [0]
         for v, d in self.dists.items():
-            if v not in visited and (t := time + d + 1) < 30:
-                results.append(v.search(visited + [self], t, score + ((30 - t) * v.flow)))
+            if v not in visited and (t := time + d + 1) < self.max_time:
+                results.append(v.search(visited + [self], t, score + ((self.max_time - t) * v.flow)))
         return max([*results, score])
 
 
-def part1():
+def get_valves(max_time):
     valves = {}
 
     for line in lines:
         name, flow, *conns = re.findall(r"([A-Z]{2}|\d+)+", line)
-        valves[name] = Valve(name, int(flow), conns)
+        valves[name] = Valve(name, int(flow), conns, max_time)
 
     while not all([all([w in v.dists for w in valves.values() if v != w]) for v in valves.values()]):
         for v in valves.values():
@@ -64,12 +66,54 @@ def part1():
             del v.dists[r]
         v.set_valves(valves)
 
+    return valves
+
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
+
+def calculate_segment(rnge):
+    valves = get_valves(26)
+
+    scores = []
+    for n in range(rnge[0], rnge[-1] + 1):
+        if n % 50 == 0:
+            print(f"{(n - rnge[0]) / (rnge[-1] - rnge[0]):.2%}")
+        if n in [2 ** x for x in range(17)]:
+            continue
+        pattern = f"{n:0{15}b}"
+
+        h, e = [], []
+        for i, (v, _) in enumerate(valves['AA'].dists.items()):
+            (h, e)[pattern[i] == '0'].append(v)
+
+        human_score = valves['AA'].search(h, 0, 0)
+        elephant_score = valves['AA'].search(e, 0, 0)
+        scores.append(human_score + elephant_score)
+
+    return max(scores)
+
+
+def part1():
+    valves = get_valves(30)
+
     return valves['AA'].search([], 0, 0)
 
 
 def part2():
-    pass
+    valves = get_valves(26)
+    pw = len(valves) - 1
+
+    ranges = split(range(1, (2 ** pw) - 1), 12)
+
+    with mp.Pool(12) as p:
+        scores = p.map(calculate_segment, ranges)
+
+    return max(scores)
 
 
-print(part1())
-print(part2())
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
