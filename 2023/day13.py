c0a5fee250d4a80f3a5f2d24b8a3fdb949569783
JReesW <j.rees.w@gmail.com>
2023-12-16 12:14:39 +0100

    day 13

--- 2023/day13.py
+++ 2023/day13.py
@@ -1,77 +1,50 @@
-with open("addenda/test.txt", 'r') as file:
+with open("addenda/input13.txt", 'r') as file:
     lines = [line.strip() for line in file.readlines()]
+
+
+def parse_grid():
+    grids = []
+
+    grid = []
+    for line in lines:
+        if len(line) == 0:
+            grids.append(grid[::])
+            grid = []
+        else:
+            grid.append(line)
+    grids.append(grid[::])
+
+    return grids
 
 
 def str_diff(a, b):
     return len([0 for x, y in zip(a, b) if x != y])
 
 
-def find_mirror(grid, flip=False, p2=False):
-    if flip:
-        grid = ["".join([row[x] for row in grid]) for x in range(len(grid[0]))]
-
-    for i, (a, b) in enumerate(zip(grid, grid[1:])):
-        if p2:
-            total_diff = 0
-            if str_diff(a, b) <= 1:
-                total_diff += str_diff(a, b)
-                for x, y in zip(grid[:i+1][::-1], grid[i+1:]):
-                    d = str_diff(x, y)
-                    total_diff += d
-                    if d >= 2:
-                        break
-                else:
-                    res = (i + 1) * (1 if flip else 100)
-                    if total_diff <= 1 and res != p2:
-                        return res
-        else:
-            if a == b:
-                for x, y in zip(grid[:i+1][::-1], grid[i+1:]):
-                    if x != y:
-                        break
-                else:
-                    return (i + 1) * (1 if flip else 100)
-
-    if not flip:
-        return find_mirror(grid, True, p2)
-    raise Exception
+def find_reflecting_row(grid, diff):
+    for i in range(1, len(grid)):
+        if sum(str_diff(a, b) for a, b in zip(grid[:i][::-1], grid[i:])) == diff:
+            return i
+    return 0
 
 
-def part1(p2=False):
-    grids = []
+def get_grid_value(grid, diff):
+    if row := find_reflecting_row(grid, diff):
+        return 100 * row
+    if col := find_reflecting_row(list(zip(*grid)), diff):
+        return col
 
-    grid = []
-    for line in lines:
-        if len(line) == 0:
-            grids.append(grid[::])
-            grid = []
-        else:
-            grid.append(line)
-    grids.append(grid[::])
 
-    res = [find_mirror(grid) for grid in grids]
+def part1():
+    grids = parse_grid()
 
-    return res if p2 else sum(res)
+    return sum(get_grid_value(grid, 0) for grid in grids)
 
 
 def part2():
-    grids = []
+    grids = parse_grid()
 
-    grid = []
-    for line in lines:
-        if len(line) == 0:
-            grids.append(grid[::])
-            grid = []
-        else:
-            grid.append(line)
-    grids.append(grid[::])
-
-    # for grid in grids:
-    #     print(find_mirror(grid))
-
-    p1 = part1(True)
-
-    return sum([find_mirror(grid, p2=p1[i]) for i, grid in enumerate(grids)])
+    return sum(get_grid_value(grid, 1) for grid in grids)
 
 
 print(part1())
